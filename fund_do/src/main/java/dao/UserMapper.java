38e98c05d83c2262818260875b9c1e09300e4a31
wangxingjie <2644949508@qq.com>
2016-02-04 17:45:37 +0800

    第一次简单的布置

---
+++ fund_do/src/main/java/dao/UserMapper.java
@@ -0,0 +1,7 @@
+package dao;
+
+/**
+ * Created by wangxingjie on 2016/2/4.
+ */
+public interface UserMapper {
+}
---
+++ 数据库/user.sql
@@ -0,0 +1,24 @@
+# Host: localhost  (Version: 5.5.11)
+# Date: 2016-02-04 16:27:53
+# Generator: MySQL-Front 5.3  (Build 4.271)
+
+/*!40101 SET NAMES gb2312 */;
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL DEFAULT '' COMMENT '用户名',
+  `userpassword` varchar(255) NOT NULL DEFAULT '' COMMENT '密码',
+  `usermail` varchar(255) DEFAULT NULL COMMENT '邮箱',
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `username` (`username`) COMMENT '用户名'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Data for table "user"
+#
+
